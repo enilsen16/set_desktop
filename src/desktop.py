0347348c12dc5880045a461f0d4678d64f7dbfcc
Erik Nilsen <enilsen16@live.com>
2017-05-22 19:58:30 +1000

    initial applescript

---
+++ src/desktop.py
@@ -0,0 +1,13 @@
+import subprocess
+
+SCRIPT = """/usr/bin/osascript<<END
+tell application "System Events"
+    tell every desktop
+        set picture to "%s"
+    end tell
+end tell
+END"""
+
+
+def set_desktop_background(filename):
+    subprocess.Popen(SCRIPT % filename, shell=True)
